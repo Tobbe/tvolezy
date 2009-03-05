4e286c1e79f4b8d241dfa588886eab52bf421b7d
Tobbe Lundberg <tobbesweden@gmail.com>
2009-03-05 14:25:27 -0500

    Whitespace changes.

--- tvolezy.h
+++ tvolezy.h
@@ -3,9 +3,10 @@
 
 #include "./lsapi.h"
 
-extern "C" {
-__declspec( dllexport ) int initModuleEx(HWND parent, HINSTANCE dll, LPCSTR szPath);
-__declspec( dllexport ) void quitModule(HINSTANCE dllInst);
+extern "C"
+{
+__declspec(dllexport) int initModuleEx(HWND parent, HINSTANCE dll, LPCSTR szPath);
+__declspec(dllexport) void quitModule(HINSTANCE dllInst);
 }
 
 #endif
